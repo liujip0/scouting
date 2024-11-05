e87cbae643e500726f7458c8b1633d576dfb5f96
liujip0 <liujip2020@gmail.com>
2024-11-04 20:14:35 -0500

    renamed pages project to isa2025

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,10 @@
-import { D1Database } from '@cloudflare/workers-types';
+import { D1Database } from "@cloudflare/workers-types";
 import {
   FetchCreateContextFnOptions,
-  fetchRequestHandler
-} from '@trpc/server/adapters/fetch';
-import { createContext } from './context.ts';
-import { appRouter } from './router.ts';
+  fetchRequestHandler,
+} from "@trpc/server/adapters/fetch";
+import { createContext } from "./context.ts";
+import { appRouter } from "./router.ts";
 
 export interface Env {
   DB: D1Database;
@@ -14,23 +14,23 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    if (request.method === 'OPTIONS') {
+    if (request.method === "OPTIONS") {
       const response = new Response(null, {
         status: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*'
-        }
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+        },
       });
       return response;
     }
 
     return fetchRequestHandler({
-      endpoint: '/api',
+      endpoint: "/",
       req: request,
       router: appRouter,
       createContext: (options: FetchCreateContextFnOptions) =>
-        createContext({ ...options, env })
+        createContext({ ...options, env }),
     });
-  }
+  },
 };
