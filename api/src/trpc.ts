3c76184fab796ef45398fcabc8a0aa65bdf54a67
liujip0 <liujip2020@gmail.com>
2024-11-04 19:32:03 -0500

    updated logging function

--- api/src/trpc.ts
+++ api/src/trpc.ts
@@ -1,5 +1,5 @@
-import { initTRPC } from '@trpc/server';
-import { Context } from './context.ts';
+import { initTRPC } from "@trpc/server";
+import { Context } from "./context.ts";
 
 const t = initTRPC.context<Context>().create();
 
@@ -10,11 +10,13 @@ export const loggedPublicProcedure = publicProcedure.use(async (opts) => {
   const start = Date.now();
   const result = await opts.next();
   const durationMs = Date.now() - start;
-  const logResult = await opts.ctx.env.DB.prepare(
-    'INSERT INTO ApiLogs(callTime, callDuration, callPath, callType, callInput, callOutput) VALUES (?, ?, ?, ?, ?, ?)'
-  )
-    .bind(start, durationMs, opts.path, opts.type, opts.input, result.ok)
-    .run();
-  console.log('Log Success:', logResult.success);
+  console.log({
+    time: start,
+    duration: durationMs,
+    apiPath: opts.path,
+    trpcType: opts.type,
+    input: opts.input,
+    result: result,
+  });
   return result;
 });
