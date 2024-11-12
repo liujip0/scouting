0ea6b38e1a7e280ff8408084f4ac79580ca13345
liujip0 <liujip2020@gmail.com>
2024-11-11 20:03:11 -0500

    changed to vite-plugin-pwa

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,32 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Indiana Scouting Alliance 2025",
+        short_name: "ISA 2025",
+        description: "Indiana Scouting Alliance App",
+        theme_color: "#000f5d",
+        //TODO: Update icons
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
