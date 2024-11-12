8f144eda46c59bf0ff1642dcb87f3f496ced1a3c
liujip0 <liujip2020@gmail.com>
2024-11-12 00:05:48 -0500

    got manifest to work, i think

--- app/src/dev-dist/sw.js
+++ app/src/dev-dist/sw.js
@@ -81,11 +81,8 @@ define(['./workbox-eaf4eecd'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
-  }, {
     "url": "index.html",
-    "revision": "0.i2grvbi8j6g"
+    "revision": "0.qhdp9ml84l8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,35 +5,19 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
   root: "./src",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    copyPublicDir: true,
   },
   plugins: [
     react(),
     VitePWA({
       registerType: "prompt",
+      manifest: false,
       devOptions: {
         enabled: true,
-      },
-      manifest: {
-        name: "Indiana Scouting Alliance 2025",
-        short_name: "ISA 2025",
-        description: "Indiana Scouting Alliance App",
-        theme_color: "#000f5d",
-        //TODO: Update icons
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
       },
     }),
   ],
