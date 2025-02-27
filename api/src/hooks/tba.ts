4b55be17da7a838916c90a4163059554464ad0d9
liujip0 <liujip2020@gmail.com>
2025-02-27 15:14:42 -0500

    i forgot request body can only be used once

--- api/src/hooks/tba.ts
+++ api/src/hooks/tba.ts
@@ -42,7 +42,7 @@ type TbaRequest =
     };
 export const tba = async (opts: WebhooksOpts): Promise<Response> => {
   const body: TbaRequest = await opts.request.json();
-  opts.env.KV.put("test", await opts.request.json());
+  opts.env.KV.put("test", body.toString());
   switch (body.message_type) {
     case "verification": {
       opts.env.KV.put(
