86008d1b2883aecdb7f12e00bdc48f9159b7e03a
liujip0 <liujip2020@gmail.com>
2025-03-11 21:30:01 -0400

    add new content popup and change ver to 1.0.0

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -7,3 +7,18 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+/// <reference types="vite-plugin-pwa/react" />
+
+declare module "virtual:pwa-register/react" {
+  import type { Dispatch, SetStateAction } from "react";
+  import type { RegisterSWOptions } from "vite-plugin-pwa/types";
+
+  export type { RegisterSWOptions };
+
+  export function useRegisterSW(options?: RegisterSWOptions): {
+    needRefresh: [boolean, Dispatch<SetStateAction<boolean>>];
+    offlineReady: [boolean, Dispatch<SetStateAction<boolean>>];
+    updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
+  };
+}
